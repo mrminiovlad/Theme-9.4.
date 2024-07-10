c5c6a2dec4b99bb74f61b51f887346f68dea5c18
Vlad <morozovvlad412@gmail.com>
2024-07-11 02:43:06 +0300

    сделал домашку

---
+++ src/components/v-cart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="v-cart">
+    <h2>Корзина</h2>
+    <vCartItem 
+      v-for="(item, index) in cart_data" 
+      :key="item.article"
+      :cart_item_data="item"
+      @deleteFromCart="deleteFromCart(index)"
+    />
+    
+  </div>
+</template>
+  
+<script>
+  import vCartItem from './v-cart-item.vue';
+  import { mapActions } from 'vuex';
+  export default {
+  name: 'v-cart',
+  components: {
+    vCartItem,
+  },
+  props:{
+    cart_data:{
+      type: Array,
+      default(){
+        return[];
+      },
+    },
+  },
+  data(){
+    return{};
+  },
+  methods:{
+    ...mapActions(['DELETE_FROM_CART']),
+    deleteFromCart(index) {
+      this.DELETE_FROM_CART(index);
+    },
+  },
+  };
+</script>
+  
+<style scoped>
+  .v-cart{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  color:gray;
+  font-size: 20px;
+  margin-bottom: 150px;
+  }
+  p{
+    text-align: center;
+    font-size: 26px;
+  }
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="v-main-wrapper">
+    <vCatalog />
+    <vCart v-if="CART.length" :cart_data="CART"/> 
+  </div>
+</template>
+
+<script>
+  import vCatalog from './v-catalog.vue';
+  import vCart from './v-cart.vue';
+  import { mapGetters } from 'vuex';
+
+  export default {
+  name: "v-main-wrapper",
+  components:{vCatalog, vCart },
+  props:{},
+  data() {
+    return{
+        title: "hello!",
+    };
+  },
+  computed:{
+    ...mapGetters (["CART"]),
+  },
+  methods:{},
+  watch:{},
+  mounted(){
+    console.log('Hello Im here!');
+  },
+  };
+</script>
+
+<style>
+.v-main-wrapper{
+    max-width: 900px;
+    margin: 0 auto;
+}
+</style>
